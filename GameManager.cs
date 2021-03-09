00de5bd89ad848305d3ff708d40f95435aa0e9e8
JOSESTEO <josesteo@gmail.com>
2021-03-09 20:26:59 +0100

    V1 Inicio

---
+++ GameManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    //Para los enumerados, antes de nada los definimos así
+    public enum GameState
+    {
+        inGame,
+        gameOver,
+        himno,
+        primerQuiz,
+        primeraParte,
+        segundoQuiz,
+        segundaparte,
+    }
+    //Y luego defino la variable que contenga el estado del enum
+    public GameState gameSate;
+    
+    private float spawnRate=1;
+    public List<GameObject> targetPrefabs;
+
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+
+    //Con esta forma mantengo los valores de score entre un mínimo y un máximo
+    private int _score;
+    private int Score
+    {
+        set
+        {
+            _score = Mathf.Clamp(value, 0, 9999);
+        }
+        get
+        {
+            return _score;
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+    
+        UpdateScore(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (gameSate == GameState.inGame)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targetPrefabs.Count);
+            Instantiate(targetPrefabs[index]);
+        }
+
+    }
+    /// <summary>
+    /// Actualiza la puntuación y la muestra por pantalla
+    /// </summary>
+    /// <param name="scoreToAdd">Número de puntos que se suman a la puntuación</param>
+    public void UpdateScore(int scoreToAdd)
+    {
+        Score += scoreToAdd; //Primero la lógica del juego
+        scoreText.text = "Puntos:\n" + Score;  //Luego la visualización
+    }
+
+    public void gameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        gameSate = GameState.gameOver;
+    }
+}
---
+++ target.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class target : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    private float minForce = 12, maxForce = 16, maxTorque = 10, xRange = 4, ySpawPos = -6;
+    
+    private GameManager _gameManager;
+    [Range(-10,50)]
+    public int pointValue;
+
+    public ParticleSystem explosionPaticle;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.AddForce(RandomForce(), ForceMode.Impulse); //Aplicamos una fuerza aleatoria entre 12 y 16
+        _rigidbody.AddTorque(RandomTorque(),RandomTorque(),RandomTorque(), ForceMode.Impulse); //Aplicamos una rotación aleatorio
+        transform.position = RandomSpawnPosition(); //Si sólo pongo dos parámetros, z=0
+        _gameManager = FindObjectOfType<GameManager>();
+    }
+/// <summary>
+/// Genera un vector aleatoria
+/// </summary>
+/// <returns>Fuerza aleatoria hacia arriba</returns>
+    private Vector3 RandomForce()
+    {
+        return Vector3.up*Random.Range(minForce, maxForce);
+    }
+/// <summary>
+/// Genera un número aleatorio
+/// </summary>
+/// <returns>Valor aleatorio entre menos MaxTorque y e</returns>
+    private float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+/// <summary>
+/// Genera una posición aleatoria
+/// </summary>
+/// <returns>Posición aleatoria en 3D con coordenada z=0</returns>
+    private Vector3 RandomSpawnPosition()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawPos);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        if (_gameManager.gameSate == GameManager.GameState.inGame)
+        {
+            Destroy(gameObject);
+            _gameManager.UpdateScore(this.pointValue);
+            Instantiate(explosionPaticle, transform.position, explosionPaticle.transform.rotation);
+            if(this.CompareTag("Bad")) _gameManager.gameOver();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        if (this.CompareTag("Good")) _gameManager.UpdateScore(-this.pointValue);
+    }
+}
